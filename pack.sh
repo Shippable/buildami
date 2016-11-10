2824cb660c0021932ee6ef851dd95b6438c32a63
Avi Cavale <avi@shippable.com>
2016-11-10 19:11:56 +0530

    adding initial test version of ami

---
+++ pack.sh
@@ -0,0 +1,85 @@
+#!/bin/bash -e
+
+export PK_INSALL_LOCATION=/opt
+export PK_VERSION=0.11.0
+export PK_FILENAME=packer_"$PK_VERSION"_linux_amd64.zip
+export RES_AWS_CREDS="aws-bits-access"
+export REPO_RESOURCE_NAME="bldami-repo"
+export RES_PARAMS=$1
+
+setup_ssh(){
+  eval `ssh-agent -s`
+  ps -eaf | grep ssh
+  ls -al ~/.ssh/
+  which ssh-agent
+}
+
+setup_keys() {
+  pushd /build/IN/$RES_AWS_CREDS
+  echo "-----------------------------------"
+
+  echo "setting AWS keys"
+  echo "-----------------------------------"
+  . integration.env
+  popd
+}
+
+setup_params(){
+  pushd ./IN/$RES_PARAMS
+  export $(jq -r '.version.propertyBag.params.REGION' version.json)
+  export $(jq -r '.version.propertyBag.params.VPC_ID' version.json)
+  export $(jq -r '.version.propertyBag.params.SUBNET_ID' version.json)
+  export $(jq -r '.version.propertyBag.params.SECURITY_GROUP_ID' version.json)
+  export $(jq -r '.version.propertyBag.params.SOURCE_AMI' version.json)
+  popd
+}
+
+install_packer() {
+  pushd $PK_INSALL_LOCATION
+  echo "Fetching packer"
+  echo "-----------------------------------"
+
+  rm -rf $PK_INSALL_LOCATION/packer
+  mkdir -p $PK_INSALL_LOCATION/packer
+
+  wget -q https://releases.hashicorp.com/packer/$PK_VERSION/"$PK_FILENAME"
+  apt-get install unzip
+  unzip -o $PK_FILENAME -d $PK_INSALL_LOCATION/packer
+  export PATH=$PATH:$PK_INSALL_LOCATION/packer
+  echo "downloaded packer successfully"
+  echo "-----------------------------------"
+  
+  local pk_version=$(packer version)
+  echo "Packer version: $pk_version"
+  popd
+}
+
+build_ami() {
+  pushd /build/IN/$REPO_RESOURCE_NAME/gitRepo
+  echo "-----------------------------------"
+
+  echo "validating AMI template"
+  echo "-----------------------------------"
+  packer validate testAMI.json
+  echo "building AMI"
+  echo "-----------------------------------"
+
+  packer build -var 'aws_access_key='$aws_access_key_id \
+    -var 'aws_secret_key='$aws_secret_access_key \
+    -var 'REGION='$REGION \
+    -var 'VPC_ID='$VPC_ID \
+    -var 'SUBNET_ID='$SUBNET_ID \
+    -var 'SECURITY_GROUP_ID='$SECURITY_GROUP_ID \
+    -var 'SOURCE_AMI='$SOURCE_AMI \
+    testAMI.json
+  popd
+}
+
+main() {
+  setup_ssh
+  setup_keys
+  install_packer
+  build_ami
+}
+
+main
