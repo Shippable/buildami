e3b763430e946c6cd69318b03e367bea62a0f07e
Avi Cavale <avi@shippable.com>
2016-11-10 19:20:29 +0530

    debug build

--- pack.sh
+++ pack.sh
@@ -7,6 +7,8 @@ export RES_AWS_CREDS="aws-bits-access"
 export REPO_RESOURCE_NAME="bldami-repo"
 export RES_PARAMS=$1
 
+echo "RES_PARAMS=$RES_PARAMS"
+
 setup_ssh(){
   eval `ssh-agent -s`
   ps -eaf | grep ssh
@@ -31,6 +33,13 @@ setup_params(){
   export $(jq -r '.version.propertyBag.params.SUBNET_ID' version.json)
   export $(jq -r '.version.propertyBag.params.SECURITY_GROUP_ID' version.json)
   export $(jq -r '.version.propertyBag.params.SOURCE_AMI' version.json)
+
+  echo "SOURCE_AMI=$SOURCE_AMI"
+  echo "VPC_ID=$VPC_ID"
+  echo "REGION=$REGION"
+  echo "SUBNET_ID=$SUBNET_ID"
+  echo "SECURITY_GROUP_ID=$SECURITY_GROUP_ID"
+
   popd
 }
 
@@ -64,14 +73,14 @@ build_ami() {
   echo "building AMI"
   echo "-----------------------------------"
 
-  packer build -var 'aws_access_key='$aws_access_key_id \
+  packer build -machine-readable -var 'aws_access_key='$aws_access_key_id \
     -var 'aws_secret_key='$aws_secret_access_key \
     -var 'REGION='$REGION \
     -var 'VPC_ID='$VPC_ID \
     -var 'SUBNET_ID='$SUBNET_ID \
     -var 'SECURITY_GROUP_ID='$SECURITY_GROUP_ID \
     -var 'SOURCE_AMI='$SOURCE_AMI \
-    testAMI.json
+    testAMI.json | awk -F, '$0 ~/artifact,0,id/ {print $6}'
   popd
 }
 
