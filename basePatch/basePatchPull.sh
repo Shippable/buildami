c59a3d48db5af8b44211748f7d9ca13052ebe760
Avi Cavale <avi@shippable.com>
2017-02-22 11:03:23 +0530

    adding cexec into patch ami

--- basePatch/basePatchPull.sh
+++ basePatch/basePatchPull.sh
@@ -1,8 +1,21 @@
 #!/bin/bash -e
 
-exec_patch() {
-  # sudo docker pull shippable/minv2:latest
-  ls
+readonly CEXEC_LOCATION_ON_HOST="/home/shippable/cexec"
+
+clean_cexec() {
+  if [ -d "$CEXEC_LOCATION_ON_HOST" ]; then
+    exec_cmd "sudo rm -rf $CEXEC_LOCATION_ON_HOST"
+  fi
 }
 
-exec_patch
+clone_cexec() {
+  mkdir -p $CEXEC_LOCATION_ON_HOST
+  sudo git clone https://github.com/Shippable/cexec.git $CEXEC_LOCATION_ON_HOST
+}
+
+main() {
+  clean_cexec
+  clone_cexec
+}
+
+main
