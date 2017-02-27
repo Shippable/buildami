f3f9a02d3bd88d3bb518d1974391bc11646e6f9e
Avi Cavale <avi@shippable.com>
2017-02-27 13:18:27 +0530

    adding image tagging and cexec,genexec pull

--- exec/execPull.sh
+++ exec/execPull.sh
@@ -1,112 +1,41 @@
 #!/bin/bash -e
 
-readonly CEXEC_LOCATION_ON_HOST="/home/shippable/cexec"
-readonly EXEC_IMAGE_NAME="shipimg/genexec"
+readonly CEXEC_LOC="/home/shippable/cexec"
+readonly GENEXEC_IMG="shipimg/genexec"
 
-# Indicates whether the script has succeeded
-export is_success=false
+set_context() {
 
-# Indicates if docker service should be restarted
-export docker_restart=false
+  echo "REL_VER=$REL_VER"
+  echo "GENEXEC_IMG=$GENEXEC_IMG"
+  echo "CEXEC_LOC=$CEXEC_LOC"
+  echo "IMAGE_NAMES_SPACED=$IMAGE_NAMES_SPACED"
 
-#
-# Prints the command start and end markers with timestamps
-# and executes the supplied command
-#
-exec_cmd() {
-  cmd=$@
-  cmd_uuid=$(python -c 'import uuid; print str(uuid.uuid4())')
-  cmd_start_timestamp=`date +"%s"`
-  echo "Running $cmd"
-  eval $cmd
-  cmd_status=$?
-  if [ "$2" ]; then
-    echo $2;
-  fi
-
-  cmd_end_timestamp=`date +"%s"`
-  if [ $cmd_status == 0 ]; then
-    echo "Completed $cmd"
-    return $cmd_status
-  else
-    echo "Failed $cmd"
-    exit 99
-  fi
+  readonly GENEXEC_IMG_WITH_TAG="$GENEXEC_IMG:$REL_VER"
 }
 
-exec_grp() {
-  group_name=$1
-  group_uuid=$(python -c 'import uuid; print str(uuid.uuid4())')
-  group_start_timestamp=`date +"%s"`
-  echo "Starting $group_name"
-  eval "$group_name"
-  group_status=$?
-  group_end_timestamp=`date +"%s"`
-  echo "Completed $group_name"
+pull_images() {
+  for IMAGE_NAME in $IMAGE_NAMES_SPACED; do
+    echo "Pulling -------------------> $IMAGE_NAME:$REL_VER"
+    sudo docker pull $IMAGE_NAME:$REL_VER
+  done
 }
 
-_run_update() {
-  is_success=false
-  update_cmd="sudo apt-get update"
-  exec_cmd "$update_cmd"
-}
-
-install_prereqs() {
-  echo "Installing prerequisite binaries"
-  _run_update
-  is_success=true
-}
-
-parse_release_version() {
-  echo "Most recent release version is : $REL_VER"
-  readonly EXEC_IMAGE_NAME_WITH_TAG="$EXEC_IMAGE_NAME:$REL_VER"
-  is_success=true
-}
-
-cleanCEXEC() {
-  is_success=false
-  if [ -d "$CEXEC_LOCATION_ON_HOST" ]; then
-    exec_cmd "sudo rm -rf $CEXEC_LOCATION_ON_HOST"
-  fi
-  is_success=true
-}
-
-cloneCEXEC() {
-  is_success=false
-  exec_cmd "sudo git clone https://github.com/Shippable/cexec.git $CEXEC_LOCATION_ON_HOST"
-  is_success=true
+tag_cexec() {
+  pushd $CEXEC_LOC
+  git pull --tags
+  git checkout $REL_VER
+  popd
 }
 
 pull_exec() {
-  is_success=false
-  exec_cmd "sudo docker pull $EXEC_IMAGE_NAME_WITH_TAG"
-  is_success=true
-}
-
-before_exit() {
-  if [ "$is_success" == true ]; then
-    echo "__SH__SCRIPT_END_SUCCESS__";
-  else
-    echo "__SH__SCRIPT_END_FAILURE__";
-  fi
+  sudo docker pull $GENEXEC_IMG_WITH_TAG
 }
 
 main() {
-  trap before_exit EXIT
-  exec_grp "install_prereqs"
-
-  trap before_exit EXIT
-  exec_grp "parse_release_version"
-
-  trap before_exit EXIT
-  exec_grp "cleanCEXEC"
-
-  trap before_exit EXIT
-  exec_grp "cloneCEXEC"
-
-  trap before_exit EXIT
-  exec_grp "pull_exec"
-pwd
+  set_context
+  pull_images
+  tag_cexec
+  pull_exec
 }
 
 main
