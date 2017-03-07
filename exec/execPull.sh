cb28fc394c87cd9c9ebdb4077698ced221d915fd
Devashish <devashish.86@gmail.com>
2017-03-07 10:30:25 -0800

    setting default value of subscription id

--- exec/execPull.sh
+++ exec/execPull.sh
@@ -122,6 +122,7 @@ update_envs() {
   ## Setting the runtime values to empty
   local default_value=""
   sed -i "s#{{LISTEN_QUEUE}}#$default_value#g" $node_env
+  sed -i "s#{{SUBSCRIPTION_ID}}#$default_value#g" $node_env
   sed -i "s#{{NODE_ID}}#$default_value#g" $node_env
   sed -i "s#{{SHIPPABLE_AMQP_URL}}#$default_value#g" $node_env
   sed -i "s#{{SHIPPABLE_API_URL}}#$default_value#g" $node_env
