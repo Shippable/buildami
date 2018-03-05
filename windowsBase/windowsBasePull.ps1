43552bd67d2d29e13a2aeb33a1ed4f886d75aa1b
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-03-05 14:41:34 +0530

    loops over images.txt for windows

--- windowsBase/windowsBasePull.ps1
+++ windowsBase/windowsBasePull.ps1
@@ -1,3 +1,11 @@
 $ErrorActionPreference = "Stop"
 
-Write-Output "TODO: Complete logic for pulling windows docker images"
+Function pull_images() {
+  echo "RES_IMG_VER_NAME=$RES_IMG_VER_NAME"
+
+  foreach ($line in Get-Content "images.txt") {
+    echo "line is $line"
+  }
+}
+
+pull_images
