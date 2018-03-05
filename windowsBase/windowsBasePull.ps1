a146565cbc084ad3084a5449bbdfa85deb904086
Vishnu Bharathi <vishnubharathi04@gmail.com>
2018-03-05 17:34:12 +0530

    Uses env instead of images.txt to pull images

--- windowsBase/windowsBasePull.ps1
+++ windowsBase/windowsBasePull.ps1
@@ -1,11 +1,11 @@
 $ErrorActionPreference = "Stop"
 
 Function pull_images() {
-  echo "RES_IMG_VER_NAME=$RES_IMG_VER_NAME"
+  echo "RES_IMG_VER_NAME=$env:RES_IMG_VER_NAME"
 
-  foreach ($IMAGE_NAME in Get-Content "images.txt") {
-    echo "Pulling -------------------> ${IMAGE_NAME}:${RES_IMG_VER_NAME}"
-    docker pull "${IMAGE_NAME}:${RES_IMG_VER_NAME}"
+  foreach ($IMAGE_NAME in $env:IMAGE_NAMES_SPACED.Split(" ")) {
+    echo "Pulling -------------------> ${IMAGE_NAME}:${env:RES_IMG_VER_NAME}"
+    docker pull "${IMAGE_NAME}:${env:RES_IMG_VER_NAME}"
   }
 }
 
