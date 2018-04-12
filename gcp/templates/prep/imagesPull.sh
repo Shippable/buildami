d34f2817ac27fe7b0a2b4eb67968ddb45e476bed
Avi Cavale <avi@shippable.com>
2018-04-11 19:00:18 -0700

    adding u14 final

--- gcp/templates/prep/imagesPull.sh
+++ gcp/templates/prep/imagesPull.sh
@@ -2,6 +2,9 @@
 pull_images() {
   echo "REL_VER=$REL_VER"
 
+  export IMAGE_NAMES_SPACED=$(eval echo $(tr '\n' ' ' < /tmp/images.txt))
+  echo $IMAGE_NAMES_SPACED
+
   for IMAGE_NAME in $IMAGE_NAMES_SPACED; do
     echo "Pulling -------------------> $IMAGE_NAME:$REL_VER"
     sudo docker pull $IMAGE_NAME:$REL_VER
