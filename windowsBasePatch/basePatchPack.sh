740e56451f60306725e61248f2c3a3827c5a081d
Ragesh Krishna <ragesh@rageshkrishna.com>
2018-05-07 22:24:30 +0530

    Enable debug for windowsBasePatch

--- windowsBasePatch/basePatchPack.sh
+++ windowsBasePatch/basePatchPack.sh
@@ -62,7 +62,7 @@ build_ami() {
   echo "building AMI"
   echo "-----------------------------------"
 
-  packer build -machine-readable -var aws_access_key=$AWS_ACCESS_KEY_ID \
+  packer build -debug -machine-readable -var aws_access_key=$AWS_ACCESS_KEY_ID \
     -var aws_secret_key=$AWS_SECRET_ACCESS_KEY \
     -var REGION=$REGION \
     -var AMI_ID=$AMI_ID \
