d52f2b3711f2618c394bc2b145c738e4af0b5d01
Avi Cavale <avi@shippable.com>
2018-05-14 09:02:33 -0700

    moving the tar download out

--- gcp/x86_64/Ubuntu_16.04/prep/init.sh
+++ gcp/x86_64/Ubuntu_16.04/prep/init.sh
@@ -4,16 +4,21 @@ set -o pipefail
 
 readonly NODE_ARCHITECTURE="$ARCHITECTURE"
 readonly NODE_OPERATING_SYSTEM="$OS"
-readonly NODE_TARBALL_URL="$NODE_TARBALL_URL"
 readonly INIT_SCRIPT_NAME="Docker_$DOCKER_VER.sh"
 
+readonly NODE_SCRIPTS_TMP_LOC="/tmp/node.tar.gz"
 readonly NODE_SCRIPTS_LOCATION="/root/node"
 readonly NODE_SHIPCTL_LOCATION="$NODE_SCRIPTS_LOCATION/shipctl"
-readonly NODE_SCRIPTS_DOWNLOAD_LOCATION="/tmp/node.tar.gz"
+
 readonly install_docker_only=true
 
 check_envs() {
-  expected_envs=$1
+    local expected_envs=(
+    'ARCHITECTURE'
+    'OS'
+    'DOCKER_VER'
+  )
+
   for env in "${expected_envs[@]}"
   do
     env_value=$(eval "echo \$$env")
@@ -56,6 +61,9 @@ __process_error() {
   echo -e "     $error"
 }
 
+__process_msg "checking environment vars"
+check_envs
+
 __process_msg "adding dns settings to the node"
 exec_cmd "echo 'supersede domain-name-servers 8.8.8.8, 8.8.4.4;' >> /etc/dhcp/dhclient.conf"
 
@@ -63,8 +71,7 @@ __process_msg "creating node scripts dir"
 exec_cmd "mkdir -p $NODE_SCRIPTS_LOCATION"
 
 __process_msg "extracting node scripts"
-exec_cmd "tar -xzvf '$NODE_SCRIPTS_DOWNLOAD_LOCATION' -C $NODE_SCRIPTS_LOCATION \
-  --strip-components=1"
+exec_cmd "tar -xzvf '$NODE_SCRIPTS_TMP_LOC' -C $NODE_SCRIPTS_LOCATION --strip-components=1"
 
 __process_msg "Initializing node"
 source "$NODE_SCRIPTS_LOCATION/initScripts/$NODE_ARCHITECTURE/$NODE_OPERATING_SYSTEM/$INIT_SCRIPT_NAME"
