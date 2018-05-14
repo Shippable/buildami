5c83b358cfd7e78c2c7d66004de6cee47282f526
Avi Cavale <avi@shippable.com>
2018-05-14 08:45:43 -0700

    moving the tar download out

--- gcp/x86_64/Ubuntu_16.04/prep/init.sh
+++ gcp/x86_64/Ubuntu_16.04/prep/init.sh
@@ -59,9 +59,6 @@ __process_error() {
 __process_msg "adding dns settings to the node"
 exec_cmd "echo 'supersede domain-name-servers 8.8.8.8, 8.8.4.4;' >> /etc/dhcp/dhclient.conf"
 
-__process_msg "downloading node scripts tarball"
-exec_cmd "wget '$NODE_TARBALL_URL' -O $NODE_SCRIPTS_DOWNLOAD_LOCATION"
-
 __process_msg "creating node scripts dir"
 exec_cmd "mkdir -p $NODE_SCRIPTS_LOCATION"
 
