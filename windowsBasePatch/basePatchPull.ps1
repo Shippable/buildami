ea26306b67dc8d87433bf589b89c563d70fa4d35
Ragesh Krishna <ragesh@rageshkrishna.com>
2018-05-21 10:46:59 +0530

    Move AWS scheduled task reset in to basePatchPull.ps1

--- windowsBasePatch/basePatchPull.ps1
+++ windowsBasePatch/basePatchPull.ps1
@@ -10,34 +10,42 @@ $REPORTS_DOWNLOAD_URL = "https://s3.amazonaws.com/shippable-artifacts/reports/$S
 
 Function clean_node_scripts() {
   if (Test-Path $NODE_SCRIPTS_LOCATION) {
-    Write-Output "Cleaning node scripts"
+    Write-Output "Cleaning node scripts at $NODE_SCRIPTS_LOCATION"
     Remove-Item -recur -force $NODE_SCRIPTS_LOCATION
   }
 }
 
 Function clone_node_scripts() {
+  Write-Output "Cloning node scripts to $NODE_SCRIPTS_LOCATION"
   mkdir -p $NODE_SCRIPTS_LOCATION
   git clone https://github.com/Shippable/node.git $NODE_SCRIPTS_LOCATION
 }
 
 Function clean_reqKick () {
   if (Test-Path $REQKICK_DIR) {
-    Write-Output "Cleaning reqKick..."
+    Write-Output "Cleaning reqKick at $REQKICK_DIR..."
     Remove-Item -recur -force $REQKICK_DIR
   }
 }
 
 Function clone_reqKick () {
-  Write-Output "Cloning reqKick..."
+  Write-Output "Cloning reqKick to $REQKICK_DIR..."
   git clone https://github.com/Shippable/reqKick.git $REQKICK_DIR
 
   cd $REQKICK_DIR
   git checkout $SHIPPABLE_RELEASE_VERSION
+  Write-Output "Running npm install for reqKick"
   npm install --quiet
 }
 
 Function pull_reqProc () {
+  Write-Output "Cloning reqProc image $REQPROC_MASTER_IMAGE"
   docker pull $REQPROC_MASTER_IMAGE
+}
+
+Function reset_aws_scheduled_task () {
+  Write-Output "Resetting AWS scheduled task"
+  & C:\ProgramData\Amazon\EC2-Windows\Launch\Scripts\InitializeInstance.ps1 -Schedule
 }
 
 Write-Output "RES_IMG_VER_NAME=$env:RES_IMG_VER_NAME"
@@ -46,3 +54,4 @@ clone_node_scripts
 clean_reqKick
 clone_reqKick
 pull_reqProc
+reset_aws_scheduled_task
