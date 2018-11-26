bf1dc43ef0db3b4ce73245a12dad821dc9a1ac29
jatindogra <dogra.jatin5@gmail.com>
2018-11-26 17:59:09 +0530

    updates u16/codeRefresh to wait for apt-get to get free

--- gcp/templates/linux/waitForApt.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-waitForApt() {
-  echo "checking for the apt-get resource"
-  time (while ps -opid= -C apt-get > /dev/null; do sleep 1m; echo 'waiting for apt-get resource to get free'; done);
-}
-
-waitForApt
